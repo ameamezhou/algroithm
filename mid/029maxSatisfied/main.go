c243d060e8fabb2b1d50963db6abe9d7f10498e3
ameamezhou <wxz17352825505@163.com>
2024-05-23 21:38:14 +0800

    he ma

---
+++ mid/029maxSatisfied/main.go
@@ -0,0 +1,14 @@
+package main
+
+// https://leetcode.cn/problems/grumpy-bookstore-owner/description/?envType=daily-question&envId=2024-04-23
+
+//func maxSatisfied(customers []int, grumpy []int, minutes int) int {
+//
+//}
+
+func main(){
+	i := 0
+	for {
+		i++
+	}
+}
